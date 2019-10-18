daaf6fd0a245ef0c436e3dd0e8b7f25a38f72432
davidhariprashad <david062796@gmail.com>
2019-10-18 12:22:52 -0400

    Add files via upload

--- timer.hpp
+++ timer.hpp
@@ -20,28 +20,28 @@ template <typename Callable, typename ... Args>
 uint64_t seconds(Callable&& f, Args&& ... args)
 {
 	typedef std::chrono::seconds Period;
-	return duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
+	return timer::duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
 }
 
 template <typename Callable, typename ... Args>
 uint64_t microseconds(Callable&& f, Args&& ... args)
 {
 	typedef std::chrono::microseconds Period;
-	return duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
+	return timer::duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
 }
 
 template <typename Callable, typename ... Args>
 uint64_t milliseconds(Callable&& f, Args&& ... args)
 {
 	typedef std::chrono::milliseconds Period;
-	return duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
+	return timer::duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
 }
 
 template <typename Callable, typename ... Args>
 uint64_t nanoseconds(Callable&& f, Args&& ... args)
 {
 	typedef std::chrono::nanoseconds Period;
-	return duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
+	return timer::duration<Period>(std::forward<Callable>(f), std::forward<Args>(args) ...).count();
 }
 
 } // namespace timer
