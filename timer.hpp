3c14da7bf1d39685a10a16dde55a4521b59e3db5
davidhariprashad <david062796@gmail.com>
2019-10-20 11:22:31 -0400

    Add files via upload

--- timer.hpp
+++ timer.hpp
@@ -14,28 +14,28 @@ std::chrono::nanoseconds elapsed_duration(Callable&& f, Args&& ... args)
 }
 
 template <typename Callable, typename ... Args>
-int64_t elapsed_seconds(Callable&& f, Args&& ... args)
+std::time_t elapsed_seconds(Callable&& f, Args&& ... args)
 {
 	return std::chrono::duration_cast<std::chrono::seconds>(elapsed_duration(
 		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
 }
 
 template <typename Callable, typename ... Args>
-int64_t elapsed_microseconds(Callable&& f, Args&& ... args)
+std::time_t elapsed_microseconds(Callable&& f, Args&& ... args)
 {
 	return std::chrono::duration_cast<std::chrono::microseconds>(elapsed_duration(
 		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
 }
 
 template <typename Callable, typename ... Args>
-int64_t elapsed_milliseconds(Callable&& f, Args&& ... args)
+std::time_t elapsed_milliseconds(Callable&& f, Args&& ... args)
 {
 	return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_duration(
 		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
 }
 
 template <typename Callable, typename ... Args>
-int64_t elapsed_nanoseconds(Callable&& f, Args&& ... args)
+std::time_t elapsed_nanoseconds(Callable&& f, Args&& ... args)
 {
 	return (elapsed_duration(std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
 }
