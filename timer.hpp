f1fc5e3db4450ce86062b81bfb4e147f9e430402
davidhariprashad <david062796@gmail.com>
2019-10-22 18:34:57 -0400

    Create timer.hpp

---
+++ timer.hpp
@@ -0,0 +1,44 @@
+#ifndef TIMER_HPP
+#define TIMER_HPP
+
+#include <chrono>
+#include <utility>
+#include <cstdint>
+
+template <typename Callable, typename ... Args>
+std::chrono::nanoseconds elapsed_duration(Callable&& f, Args&& ... args)
+{
+	const std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds> begin = std::chrono::steady_clock::now();
+	f(std::forward<Args>(args) ...);
+	const std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds> end = std::chrono::steady_clock::now();
+	return (end - begin);
+}
+
+template <typename Callable, typename ... Args>
+int64_t elapsed_seconds(Callable&& f, Args&& ... args)
+{
+	return std::chrono::duration_cast<std::chrono::seconds>(elapsed_duration(
+		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
+}
+
+template <typename Callable, typename ... Args>
+int64_t elapsed_milliseconds(Callable&& f, Args&& ... args)
+{
+	return std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_duration(
+		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
+}
+
+template <typename Callable, typename ... Args>
+int64_t elapsed_microseconds(Callable&& f, Args&& ... args)
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(elapsed_duration(
+		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
+}
+template <typename Callable, typename ... Args>
+int64_t elapsed_nanoseconds(Callable&& f, Args&& ... args)
+{
+	return std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed_duration(
+		std::forward<Callable>(f), std::forward<Args>(args) ...)).count();
+}
+
+#endif
